f59813ee545e304d757f66798cca4f699ad3ea9d
adetola-dotbat <adewolenoah4@gmail.com>
2025-01-30 19:20:33 +0100

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\GreetingController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return response()->json([
+        'email' => 'adewolenoah3@gmail.com',
+        'current_datetime' => now()->toIso8601String(),
+        'github_url' => 'https://github.com/adetola-dotbat'
+    ], 200);
+});
