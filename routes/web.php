4f2f3e406f19f1c33d43e2b20c723e3105c9f57a
adetola-dotbat <adewolenoah4@gmail.com>
2025-01-31 05:32:47 +0100

    changed to based url

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'email' => 'adewolenoah3@gmail.com',
+        'current_datetime' => now()->toIso8601String(),
+        'github_url' => 'https://github.com/adetola-dotbat'
+    ], 200);
 });
